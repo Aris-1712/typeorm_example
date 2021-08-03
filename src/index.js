0f621759148c8cd5bb864d8398e01421a89e48b8
aris abdulgani <aris.gani@gmail.com>
2021-08-03 15:27:23 +0400

    initial

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 import {createConnection} from "typeorm";
 import {Post} from "./entity/Post";
 import {Category} from "./entity/Category";
+import Express from 'express'
+const app=Express()
+createConnection().then(connection=>{
+    const postRepo = connection.getRepository(Post);
+    app.get('/',async(req,res)=>{
+        let result=await postRepo.find()
+        res.send(result)
+    })
+})
+app.listen(3555,()=>{
+    console.log("Listening")
+})
 
-// connection settings are in the "ormconfig.json" file
-createConnection().then(connection => {
-
-    const category1 = new Category();
-    category1.name = "TypeScript";
-
-    const category2 = new Category();
-    category2.name = "Programming";
-
-    const post = new Post();
-    post.title = "Control flow based type analysis";
-    post.text = "TypeScript 2.0 implements a control flow-based type analysis for local variables and parameters.";
-    post.categories = [category1, category2];
-
-    return connection
-        .getRepository(Post)
-        .save(post)
-        .then(post => {
-            console.log("Post has been saved: ", post);
-        });
-
-}).catch(error => console.log("Error: ", error));
